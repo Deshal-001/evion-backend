f4c98318d03b4e05fae3f44b9e15aca369849d75
Kavindu Deshal <85722430+Deshal-001@users.noreply.github.com>
2025-09-07 12:35:49 +0200

    first commit

---
+++ src/main/java/com/evion/evion_backend/EvionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.evion.evion_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evion/evion_backend/EvionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evion.evion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
